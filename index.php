b7e74c52b1b4fe48a6a9d9df5986467a8261b5e5
BhasinParth <parthbhasin96@gmail.com>
2016-03-14 21:08:45 +0530

    Added files via upload

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>bestworstfilm.com</title>
+		<link rel="stylesheet" type="text/css" href="css/default.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<script src="js/modernizr.custom.js"></script>
+		<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+        <script type="text/javascript" src="engine1/jquery.js"></script>
+	<body>
+		<div class="container">
+			<!--<header class="clearfix">
+			<a href="pan.html">
+			   <img src="images/pan.jpg" alt="Pan Picture" width ="1370" height ="300">
+            </a>	
+			</header>-->
+		    <div class="logreg">
+			      <ul>
+				     <li><a href="login.html">Login</a></li>
+					 <li><a href="register.html">Register</a></li>
+				 </ul>
+		    </div>
+           <!-- <div class="logo" id="logo">
+			    
+			    <a href="#">
+			     <img src="images/logo.png" alt="logo" width ="235" height ="55" >
+				</a>
+            </div>-->			
+			<div class="main">
+				<nav id="cbp-hrmenu"class="cbp-hrmenu" role="navigation">
+					<ul>
+					    <li>
+						    <a href="home.html">
+			                  <img src="images/logo.png" alt="logo" width ="235" height ="55" >
+				            </a>
+						</li>
+						&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+						&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+						&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+						&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+						
+						
+						<li>
+							<a href="#">MOVIES</a>
+							<div class="cbp-hrsub">
+								<div class="cbp-hrsub-inner"> 
+									<div>
+										<h4>NOW PLAYING</h4>
+										<ul>
+											<li><a href="shaan.html">Shaandaar (UA)</a></li>
+											<li><a href="#">Pyaar Ka Punchnama 2 (A)</a></li>
+											<li><a href="#">Singh is Bliing (UA)</a></li>
+											<li><a href="#">The Martian (UA)</a></li>
+											<li><a href="#">Black Mass (A)</a></li>
+											<li><a href="#">The Intern (UA)</a></li>										
+										</ul>
+									</div>
+									<div>
+										<h4>COMING SOON</h4>
+										<ul>
+											<li><a href="#">Prem Ratan Dhan Payo</a></li>
+											<li><a href="#">BajiRao Mastani</a></li>
+											<li><a href="#">Burnt</a></li>
+											<li><a href="#">The Hunger Games: Mockingjay - Part 2</a></li>
+											<li><a href="star.html">Star Wars: The Force Awakens</a></li>
+										</ul>
+									</div>
+									<div>
+										<h4>EXPLORE MORE</h4>
+										<ul>
+											<li><a href="http://www.movieclips.com/">WATCH TRAILERS</a></li>
+											<li><a href="#">TOP BOX OFFICE</a></li>
+											<li><a href="#">NEW DVDs</a></li>
+										</ul>
+										
+									</div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<a href="#">THEATRES</a>
+							<div class="cbp-hrsub">
+								<div class="cbp-hrsub-inner"> 
+									<div>
+										<h4>Delhi</h4>
+										<ul>
+											<li><a href="#">BIG Cinemas: Odeon</a></li>
+											<li><a href="#">DT Star Cinemas: Saket</a></li>
+											<li><a href="#">Eros One: Jangpura Extn</a></li>
+											<li><a href="#">Fun Cinemas: V3S Mall, Laxminagar</a></li>
+											<li><a href="#">PVR: Rivoli</a></li>
+											<li><a href="#">Satyam Cineplexes: Nehru Place</a></li>
+										</ul>
+									</div>
+									<div>
+										<h4>Noida</h4>
+										<ul>
+									        <li><a href="#">BIG Cinemas: Ansal, Greater Noida</a></li>
+											<li><a href="#">INOX: MSX Mall, Greater Noida</a></li>
+											<li><a href="#">Movietime: Dharam Palace Mall</a></li>
+											<li><a href="#">Spice Cinemas: Noida</a></li>
+											<li><a href="#">Wave: Noida</a></li>
+										</ul>
+									</div>
+									<div>
+										<h4>Ghaziabad</h4>
+										<ul>
+											<li><a href="#">Chaudhry Cineplex: Ghaziabad</a></li>
+											<li><a href="#">Fun Cinemas: World Square Mall, Ghaziabad</a></li>
+											<li><a href="#">PVR: Opulent Mall, Ghaziabad</a></li>
+											<li><a href="#">Star X Cinema: Vaishali</a></li>
+											<li><a href="#">Wave: RDC, Ghaziabad</a></li>
+											<li><a href="#">Wave: The Wave Mall, Kaushambi</a></li>
+										</ul>			
+									</div>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div class="movtickets" >
+			    <div class="movticket" >
+			      <h2><b>&nbsp&nbsp&nbsp&nbspBUY MOVIE TICKETS</b></h2>
+                </div>
+			    <div id="grpimage" class="grpimage">  
+                 <ul>			
+			        &nbsp&nbsp&nbsp&nbsp
+			         <li><a href="#"><img src="images/pyaargrp.jpg" alt="PyaarPanchnama Picture" width="150" height="225"></a></li>
+                     <li><a href="#"><img src="images/shanngrp.jpg" alt="Shaandaar Picture" width="150" height="225"></a></li>
+	                 <li><a href="#"><img src="images/bajiraogrp.jpg" alt="BajiraoMastani Picture" width="150" height="225"></a></li>			
+                     <li><a href="#"><img src="images/martiangrp.jpg" alt="TheMartian Picture" width="150" height="225"></a></li>
+                     <li><a href="#"><img src="images/pangrp.jpg" alt="Pan Picture" width="150" height="225"></a></li>
+			         <li><a href="#"><img src="images/hungergrp.jpg" alt="HungerGames Picture" width="150" height="225"></a></li>
+                     <li><a href="#"><img src="images/burntgrp.jpg" alt="Burnt Picture" width="150" height="225"></a></li>
+                     <li><a href="#"><img src="images/theinterngrp.jpg" alt="Intern Picture" width="150" height="225"></a></li> 	
+				 </ul>	 
+                </div>
+			   </br>
+			</div>
+			
+			
+            
+            <div id="wowslider-container1">
+		         <div class="trailer" >
+			        <h1><b>&nbsp&nbsp&nbsp&nbspWATCH TRAILERS</b></h1>
+                 </div>
+				 </br>
+              <div class="ws_images">
+					<ul>
+						<li><a href="https://youtu.be/tC-HGUhxyyc" target="_blank"><img src="data1/images/pyaar.jpg" alt="" title="" id="wows1_0"/></a></li>
+						<li><a href="https://youtu.be/k99-vMPh3-A" target="_blank"><img src="data1/images/shaandaar.jpg" alt="" title="" id="wows1_1"/></a></li>
+						<li><a href="https://youtu.be/Vd4iNPuRlx4" target="_blank"><img src="data1/images/premratan.jpg" alt="" title="" id="wows1_2"/></a></li>
+						<li><a href="https://youtu.be/ej3ioOneTy8" target="_blank"><img src="data1/images/martian.jpg" alt="" title="" id="wows1_3"/></a></li>
+						<li><a href="https://youtu.be/ZU3Xban0Y6A" target="_blank"><img src="data1/images/intern.jpg" alt="" title="" id="wows1_4"/></a></li>
+						<li><a href="https://youtu.be/sGbxmsDFVnE" target="_blank"><img src="data1/images/starwars.jpg" alt="" title="" id="wows1_5"/></a></li>
+					</ul>
+			  </div>
+			  <div class="ws_bullets">
+			   <div>
+				<a href="#" title=""><span><img src="data1/tooltips/pyaar.jpg" alt=""/>1</span></a>
+				<a href="#" title=""><span><img src="data1/tooltips/shaandaar.jpg" alt=""/>2</span></a>
+				<a href="#" title=""><span><img src="data1/tooltips/premratan.jpg" alt=""/>3</span></a>
+				<a href="#" title=""><span><img src="data1/tooltips/martian.jpg" alt=""/>4</span></a>
+				<a href="#" title=""><span><img src="data1/tooltips/intern.jpg" alt=""/>5</span></a>
+				<a href="#" title=""><span><img src="data1/tooltips/starwars.jpg" alt=""/>6</span></a>
+			   </div>
+			  </div>
+
+            </div>	
+                <a href="paranomal.html">
+			       <img src="images/paranomal.jpg" alt="Paranomal Picture" width ="1370" height ="300">
+                </a>
+	        <div class="offers">
+	 		    <div class="offer" >
+			        <h2><b>&nbsp&nbsp&nbsp&nbspOFFERS</b></h2>
+                </div>
+				<div id="grpimage" class="grpimage">  
+                    <ul>			
+					    &nbsp&nbsp
+						<li><a href="#"><img src="images/payumoney.jpg" alt="PayuMoney Picture" ></a></br> MOVIE AT 10% CASHBACK USING </br>PAYUMONEY (ONLY ON MONDAY)</br></br>OFFER IS VALID FOR LIMITED PERIOD </li>
+						<li><a href="#"><img src="images/icici.jpg" alt="Icici Picture" ></a></br>ICICI OFFER 30% DISCOUNT MAX 150 RS</br>OFFER TILL 31 OCT 2015</br></br>PROMO CODE :&nbsp IC150 </li>
+						<li><a href="#"><img src="images/axisdebitcard.jpg" alt="Axis Picture"></a></br>AXIS BANK DEBIT CARD OFFER</br> BUY 1 GET 1 OFFER TILL 31 JAN 2016</br></br>PROMO CODE : &nbsp AXISFT01 </li>
+						<li><a href="#"><img src="images/funcinema.jpg" alt="FunCinema Picture"></a></br>FUN CINEMAS BUY 1 GET 1 OFFER</br>OFFER IS VALID FOR LIMITED PERIOD</br></br>PROMO CODE : &nbsp FUN200</li>	
+					</ul>	
+				</div>
+		    </div>
+			<div class="newcoms" >
+			    <div class="newcom" >
+			      <h2><b>&nbsp&nbsp&nbsp&nbspNEW+COMING SOON</b></h2>
+                </div>
+			    <div id="grpimage" class="grpimage">  
+                 <ul>			
+			        &nbsp&nbsp&nbsp&nbsp
+                     <li><a href="#"><img src="images/spectreposter.jpg" alt="spectre Picture" width="150" height="225"></a></li>					
+			         <li><a href="#"><img src="images/kungfupanda3poster.jpg" alt="kungfupanda3 Picture" width="150" height="225"></a></li>
+	                 <li><a href="#"><img src="images/peanutsposter.jpg" alt="peanuts Picture" width="150" height="225"></a></li>
+                     <li><a href="#"><img src="images/burntgrp.jpg" alt="Burnt Picture" width="150" height="225"></a></li>					 
+                     <li><a href="#"><img src="images/starwarsgrp.jpg" alt="StarWars Picture" width="150" height="225"></a></li> 
+                     <li><a href="#"><img src="images/hoteltrans2poster.jpg" alt="hoteltrans2 Picture" width="150" height="225"></a></li>
+			         <li><a href="#"><img src="images/hungergrp.jpg" alt="HungerGames Picture" width="150" height="225"></a></li>
+                     <li><a href="#"><img src="images/dinosaurposter.jpg" alt="dinosaur Picture" width="150" height="225"></a></li>	
+                 </ul>					 
+                </div>
+			   </br>
+			</div>			
+		</div>	
+		
+		<footer>
+		<a href="#">
+			   <img src="images/footer.png" alt="No Picture" >
+            </a>	
+		</footer>
+		<script type="text/javascript" src="engine1/wowslider.js"></script>
+        <script type="text/javascript" src="engine1/script.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="js/cbpHorizontalMenu.min.js"></script>
+		<script>
+			$(function() {
+				cbpHorizontalMenu.init();
+			});
+		</script>
+	</body>
+</html>
